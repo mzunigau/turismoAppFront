a0a8080ae64f291cce6aed7a08b1880f7f641136
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-04-23 01:22:09 +0000

    Definicion de los modelos preliminares

--- src/api/models.py
+++ src/api/models.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 roles_usuarios= db.Table('roles_usuarios', db.Column("usuario_id", db.Integer, db.ForeignKey("usuarios.id")),db.Column("role_id", db.Integer, db.ForeignKey("roles.id")))
+sitios_usuarios= db.Table('sitios_usuarios', db.Column("usuario_id", db.Integer, db.ForeignKey("usuarios.id")),db.Column("sitio_id", db.Integer, db.ForeignKey("sitios.id")))
+
 categorias_usuarios= db.Table('categorias_usuarios', db.Column("usuario_id", db.Integer, db.ForeignKey("usuarios.id")),db.Column("categoria_id", db.Integer, db.ForeignKey("categorias.id")))
 categorias_sitios= db.Table('categorias_sitios', db.Column("sitio_id", db.Integer, db.ForeignKey("sitios.id")),db.Column("categoria_id", db.Integer, db.ForeignKey("categorias.id")))
 
@@ -12,10 +14,13 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     edad = db.Column(db.Integer)
     roles = db.relationship("Rol", secondary= roles_usuarios)
     categorias = db.relationship("Categoria", secondary= categorias_usuarios)
+    sitios_favoritos = db.relationship("Sitio", secondary= sitios_usuarios)
+    comentarios = db.relationship("Comentario")
+    calificaciones = db.relationship("Calificacion")
     
 
     def __repr__(self):
@@ -26,33 +31,39 @@ class Usuario(db.Model):
             "id": self.id,
             "email": self.email,
             "nombre": self.nombre,
-            "edad": self.edad
+            "edad": self.edad,
+            "roles": list(map(lambda x: x.serialize(), self.roles)),
+            "categorias": list(map(lambda x: x.serialize(), self.categorias))
             # do not serialize the password, its a security breach
         }
 class Sitio(db.Model):
     __tablename__='sitios'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(120), nullable=False)
-    edad = db.Column(db.Integer)
-    categorias = db.relationship("Categoria", secondary= categorias_usuarios)
-    dificultad_id = Column(Integer, ForeignKey('dificultades.id'))
+    categorias = db.relationship("Categoria", secondary= categorias_sitios)
+    dificultad_id = db.Column(db.Integer, db.ForeignKey('dificultades.id'))
+    provincia_id = db.Column(db.Integer, db.ForeignKey('provincias.id'))
+    comentarios = db.relationship("Comentario")
+    calificaciones = db.relationship("Calificacion")
 
     def __repr__(self):
-        return '<Usuario %r>' % self.nombre
+        return '<Sitio %r>' % self.nombre
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
             "nombre": self.nombre,
-            "edad": self.edad
+            "dificultad_id": self.dificultad_id,
+            "provincia_id": self.provincia_id,
+            "categorias": list(map(lambda x: x.serialize(), self.categorias))
             # do not serialize the password, its a security breach
         }
 
 class Provincia(db.Model):
     __tablename__='provincias'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    nombre = db.Column(db.String(120), nullable=False)
+    sitios = db.relationship("Sitio")
 
     def __repr__(self):
         return '<Provincia %r>' % self.nombre
@@ -60,14 +71,15 @@ class Provincia(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "nombre": self.nombre            
+            "nombre": self.nombre,
+            "sitios": list(map(lambda x: x.serialize(), self.sitios))           
         }
 
 class Dificultad(db.Model):
     __tablename__='dificultades'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(120), unique=True, nullable=False)
-    sitios = relationship("Sitio")
+    nombre = db.Column(db.String(120), nullable=False)
+    sitios = db.relationship("Sitio")
 
     def __repr__(self):
         return '<Dificultad %r>' % self.nombre
@@ -75,16 +87,17 @@ class Dificultad(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "nombre": self.nombre            
+            "nombre": self.nombre,
+            "sitios": list(map(lambda x: x.serialize(), self.sitios))           
         }
 
 class Rol(db.Model):
     __tablename__='roles'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    nombre = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
-        return '<Roles %r>' % self.nombre
+        return '<Rol %r>' % self.nombre
 
     def serialize(self):
         return {
@@ -95,15 +108,55 @@ class Rol(db.Model):
 class Categoria(db.Model):
     __tablename__='categorias'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    nombre = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
-        return '<Categorias %r>' % self.nombre
+        return '<Categoria %r>' % self.nombre
 
     def serialize(self):
         return {
             "id": self.id,
             "nombre": self.nombre            
+        }
+
+class Comentario(db.Model):
+    __tablename__='comentarios'
+    id = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(120), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    sitio_id = db.Column(db.Integer, db.ForeignKey('sitios.id'))
+    fecha = db.Column(db.Date,nullable=False)
+
+    def __repr__(self):
+        return '<Comentario %r>' % self.nombre
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "descripcion": self.nombre,
+            "usuario_id": self.usuario_id,
+            "sitio_id": self.sitio_id,
+            "fecha": self.fecha           
+        }
+
+class Calificion(db.Model):
+    __tablename__='calificaciones'
+    id = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(120), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    sitio_id = db.Column(db.Integer, db.ForeignKey('sitios.id'))
+    numero = db.Column(db.Integer,nullable=False)
+
+    def __repr__(self):
+        return '<Comentario %r>' % self.nombre
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "descripcion": self.nombre,
+            "usuario_id": self.usuario_id,
+            "sitio_id": self.sitio_id,
+            "numero": self.numero           
         }
 
 
