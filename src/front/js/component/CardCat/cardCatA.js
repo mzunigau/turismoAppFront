ba921bf2428decdf454db2d1640f0e7536614c94
DiegoPS-ZRR <pacheco.luisdiego@gmail.com>
2021-05-01 22:23:31 +0000

    Sabado 422pm

--- src/front/js/component/CardCat/cardCatA.js
+++ src/front/js/component/CardCat/cardCatA.js
@@ -11,17 +11,14 @@ const CardCatA = () => {
 					src="https://www.procomer.com/wp-content/uploads/2020/01/porque_costarica.jpg"
 				/>
 				<Card.Body>
-					<Card.Title>Playa</Card.Title>
+					<Card.Title>
+						<Link to="/homeCatDest">Playa</Link>
+					</Card.Title>
 					<Card.Text>
 						This is a wider card with supporting text below as a natural lead-in to additional content. This
 						content is a little bit longer.
 					</Card.Text>
 				</Card.Body>
-				<Card.Footer>
-					<Link to="/homeCatDest">
-						<Button variant="primary">Ir a Playa</Button>
-					</Link>
-				</Card.Footer>
 			</Card>
 
 			<Card>
@@ -30,14 +27,13 @@ const CardCatA = () => {
 					src="https://www.procomer.com/wp-content/uploads/2020/01/porque_costarica.jpg"
 				/>
 				<Card.Body>
-					<Card.Title>Montaña</Card.Title>
+					<Card.Title>
+						<Link to="/homeCatDest">Montaña</Link>
+					</Card.Title>
 					<Card.Text>
 						This card has supporting text below as a natural lead-in to additional content.{" "}
 					</Card.Text>
 				</Card.Body>
-				<Card.Footer>
-					<Button variant="primary">Ir a Montaña</Button>
-				</Card.Footer>
 			</Card>
 
 			<Card>
@@ -46,15 +42,14 @@ const CardCatA = () => {
 					src="https://www.procomer.com/wp-content/uploads/2020/01/porque_costarica.jpg"
 				/>
 				<Card.Body>
-					<Card.Title>Ríos</Card.Title>
+					<Card.Title>
+						<Link to="/homeCatDest">Ríos</Link>
+					</Card.Title>
 					<Card.Text>
 						This is a wider card with supporting text below as a natural lead-in to additional content. This
 						card has even longer content than the first to show that equal height action.
 					</Card.Text>
 				</Card.Body>
-				<Card.Footer>
-					<Button variant="primary">Ir a Ríos</Button>
-				</Card.Footer>
 			</Card>
 		</CardDeck>
 	);
--- src/front/js/component/navbarIn.js
+++ src/front/js/component/navbarIn.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Image } from "react-bootstrap"
 import SearchBar from "./SearchBar/searchBar";
 import DropLogin from "./DropLogin/dropLogin";
+import LogoApp from "../../img/CRideLogo.png"
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">CRide</span>
+				{/* <span className="navbar-brand mb-0 h1">CRide</span> */}
+                <Image src={LogoApp} width="200px" height="200px" />
 			</Link>
 
 			<SearchBar />
